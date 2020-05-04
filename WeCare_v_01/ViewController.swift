3cef75c90f7a11813eb50a649acd673cc543bef9
Mikail Masood <50792378+mikail-masood@users.noreply.github.com>
2020-05-04 05:25:13 -0400

    Initial Commit

---
+++ WeCare_v_01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeCare_v_01
+//
+//  Created by Mikail Masood on 2020-05-04.
+//  Copyright © 2020 Mikail Masood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
